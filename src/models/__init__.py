c77014f75abe9d4fda654f526acc45e967c4cb0a
antohus-liga <antonioslotylo0904@gmail.com>
2026-02-28 16:22:37 +0000

    added __init__.py to repos and models

---
+++ src/models/__init__.py
@@ -0,0 +1,9 @@
+from .client_model import Client
+from .material_model import MaterialRecord, MaterialView
+from .supplier_model import Supplier
+from .production_line_model import ProductionLineRecord, ProductionLineView
+from .product_model import ProductView, ProductRecord
+from .product_materials_model import ProductMaterials
+from .movement_out_model import MovementOutRecord, MovementOutView
+from .movement_in_model import MovementInRecord, MovementInView
+from .enums import ClientType, BaseUnit, SupplierType, MaterialCategory, ProductCategory
