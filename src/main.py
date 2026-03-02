4f0394ffafab9023dbca39a602dfc4f4f8ad505f
antohus-liga <antonioslotylo0904@gmail.com>
2026-03-02 19:59:23 +0000

    appstate is loaded on app start

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import QApplication
 import sys
 
+from core import data_manager
 from repos.base_repo import Base
 from ui.main_window import MainWindow
-
 
 app = QApplication(sys.argv)
 
@@ -11,6 +11,9 @@ with open("src/main.qss") as style:
     app.setStyleSheet(style.read())
 
 base = Base("test.db")
+data_manager = data_manager.DataManager()
+data_manager.refresh_all()
+
 main_window = MainWindow()
 main_window.show()
 
