240334f1604059843e39e271eb79455c2a8de9c5
antohus-liga <antonioslotylo0904@gmail.com>
2026-03-08 16:29:50 +0000

    delete method rewrite

--- src/ui/prod_mat_widget.py
+++ src/ui/prod_mat_widget.py
@@ -1,13 +1,8 @@
 import json
 from PySide6.QtCore import Qt, Signal
-from PySide6.QtWidgets import (
-    QHBoxLayout,
-    QMessageBox,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide6.QtWidgets import QHBoxLayout, QPushButton, QVBoxLayout, QWidget
 
+from core.appstate import AppState
 from core.data_manager import DataManager
 from ui.containers.inputs_container import InputsContainer
 from ui.views.tree_widget import TreeWidget
@@ -62,35 +57,28 @@ class ProdMatWidget(QWidget):
         self.data_manager.refresh_all()
 
     def delete_values(self):
-        db = QSqlDatabase.database()
-        query = QSqlQuery()
-
         items = self.tree.selectedItems()
         for item in items:
             parent = item.parent()
 
+            records_to_delete = []
             if parent is not None:
-                query.prepare(
-                    "DELETE FROM product_materials WHERE pro_code = ? AND mat_code = ?"
-                )
-                query.addBindValue(parent.text(0))
-                query.addBindValue(item.text(0))
+                pro_code = parent.text(0)
+                mat_code = item.text(0)
+                records_to_delete = [
+                    record
+                    for record in list(AppState.product_materials.values())
+                    if record.pro_code == pro_code and record.mat_code == mat_code
+                ]
+                print("hi from if")
             else:
-                query.prepare("DELETE FROM product_materials WHERE pro_code = ?")
-                query.addBindValue(item.text(0))
+                pro_code = item.text(0)
+                records_to_delete = [
+                    record
+                    for record in list(AppState.product_materials.values())
+                    if record.pro_code == pro_code
+                ]
+                print("hi from else")
+            print(records_to_delete)
 
-        if not db.transaction():
-            return
-        try:
-            query.exec()
-
-            db.commit()
-            self.data_changed.emit()
-
-        except Exception as e:
-            db.rollback()
-            QMessageBox.critical(
-                None, "Operacao nao concluida", f"Insercao cancelada: {str(e)}"
-            )
-        finally:
-            self.tree.load()
+        self.tree.load()
